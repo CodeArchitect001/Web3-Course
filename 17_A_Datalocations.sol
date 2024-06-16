5bd3d6242e90494c517057bdf08171822725a451
CodeArchitect001 <164287215+CodeArchitect001@users.noreply.github.com>
2024-06-16 10:44:27 +0800

    Delete 17_A_Datalocations.sol
    test17

--- 17_A_Datalocations.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.24;
-
-import "./17_B_StructDeclaration.sol";
-    contract Todos {
-        // An array of 'Todo' structs
-        Todo[] public todos;
-    }
